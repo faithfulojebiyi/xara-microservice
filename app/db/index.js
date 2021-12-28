6c72f50e7e15b5695136dbe035d3de783105cf8b
Faithful Ojebiyi <faithfulojebiyi@gmail.com>
2021-12-28 22:21:34 +0100

    FIx:- (Remove duplicate db pool connection)

--- app/db/index.js
+++ app/db/index.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose')
 const config = require('../../config/env')
-mongoose.connect(config.DATABASE_URL).then(() => {
-  logger.info('Connected to database')
-}).catch(err => {
-  logger.info(`error connecting to database, ${err.message}`)
-})
-
-const db = mongoose.connection
+const db = mongoose.createConnection(config.DATABASE_URL)
 
 module.exports = db
